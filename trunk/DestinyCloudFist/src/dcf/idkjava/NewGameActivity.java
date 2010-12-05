c5d48c1206e428091ac0eca47f35e8f72a9084ab
GurtejKanwar <GurtejKanwar@010c4ee4-ec02-a103-3074-47c09ac6e333>
2010-12-05 00:23:43 +0000

    Moved the stuff in LoadGameActivity to NewGameActivity and added an array to keep track of the views.

--- trunk/DestinyCloudFist/src/dcf/idkjava/NewGameActivity.java
+++ trunk/DestinyCloudFist/src/dcf/idkjava/NewGameActivity.java
@@ -2,6 +2,7 @@ package dcf.idkjava;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewGameActivity extends Activity
 {
@@ -12,5 +13,26 @@ public class NewGameActivity extends Activity
 		setContentView(R.layout.newgame);
 	}
 	
+	public void onFileSelected()
+	{
+		View[] playerOptionViews;
+		//Get filename
+		//Read file and find max players
+		
+		//Loop through players and create a new view for each one
+		playerOptionViews = new View[5/*numPlayers*/];
+		for(int i = 0; i < 5/*numPlayers*/; i++)
+		{
+			playerOptionViews[i] = createPlayerOptionsView(i);
+		}
+	}
 	
+	public View createPlayerOptionsView(int viewNum)
+	{
+		View temp = getLayoutInflater().inflate(R.layout.playeroption, null);
+		addContentView(temp, null);
+		
+		temp.setId(viewNum);
+		return temp;
+	}
 }
