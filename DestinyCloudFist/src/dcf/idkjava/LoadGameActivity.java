15f2f817eb5c788f16ba5da76af7628cdee605e7
GurtejKanwar <GurtejKanwar@010c4ee4-ec02-a103-3074-47c09ac6e333>
2010-12-05 00:23:43 +0000

    Moved the stuff in LoadGameActivity to NewGameActivity and added an array to keep track of the views.

--- DestinyCloudFist/src/dcf/idkjava/LoadGameActivity.java
+++ DestinyCloudFist/src/dcf/idkjava/LoadGameActivity.java
@@ -11,20 +11,4 @@ public class LoadGameActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.loadgame);
 	}
-	
-	public void onFileSelected()
-	{
-		//Get filename
-		//Read file and find max players
-		//Loop through players and create a new view for each one
-		for(int i = 0; i < 5/*numPlayers*/; i++)
-		{
-			createPlayerOptionsView(i);
-		}
-	}
-	
-	public void createPlayerOptionsView(int viewNum)
-	{
-		addContentView(getLayoutInflater().inflate(R.layout.playeroption, null), null);
-	}
 }
