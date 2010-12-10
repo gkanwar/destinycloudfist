e1b04ac73ba213cf26b6022ad59a3bd8b86f4bb7
GurtejKanwar <GurtejKanwar@010c4ee4-ec02-a103-3074-47c09ac6e333>
2010-12-10 01:21:46 +0000

    Set up an options menu in the GameActivity. Create a GameOverActivity to be created when the game ends normally and added functions for saveAndQuit and just quit from the options menu.

--- trunk/DestinyCloudFist/src/dcf/idkjava/GameActivity.java
+++ trunk/DestinyCloudFist/src/dcf/idkjava/GameActivity.java
@@ -2,17 +2,22 @@ package dcf.idkjava;
 
 import dcf.idkjava.main.Game;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class GameActivity extends Activity
 {
 	Game m_game;
+	Menu m_menu;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game);
+		getMenuInflater().inflate(R.menu.optionsmenu, m_menu);
 		
 		loop();
 	}
@@ -23,5 +28,34 @@ public class GameActivity extends Activity
 		{
 			m_game.mainLoop();
 		}
+		
+		startActivity(new Intent(this, GameOverActivity.class));
+		finish();
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+			case R.id.saveandquit:
+				saveAndQuit();
+				return true;
+			case R.id.quit:
+				quit();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	private void saveAndQuit()
+	{
+		//TODO: Implement saving stuff here
+		finish();
+	}
+	private void quit()
+	{
+		finish();
 	}
 }
---
+++ trunk/DestinyCloudFist/src/dcf/idkjava/GameOverActivity.java
@@ -0,0 +1,14 @@
+package dcf.idkjava;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameOverActivity extends Activity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gameover);
+	}
+}
