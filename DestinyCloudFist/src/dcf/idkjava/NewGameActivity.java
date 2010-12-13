c170f1202d443273c94db909b84a6d538989bc97
GurtejKanwar <GurtejKanwar@010c4ee4-ec02-a103-3074-47c09ac6e333>
2010-12-13 17:14:53 +0000

    Working on NewGameActivity.

--- DestinyCloudFist/src/dcf/idkjava/NewGameActivity.java
+++ DestinyCloudFist/src/dcf/idkjava/NewGameActivity.java
@@ -1,11 +1,25 @@
 package dcf.idkjava;
 
+import java.util.ArrayList;
+
+import dcf.idkjava.main.Game;
+import dcf.idkjava.main.Player;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class NewGameActivity extends Activity
 {
+	//The number of player for the map
+	private int m_numPlayers = 5;
+	//Array of player name views
+	private ArrayList<EditText> m_names = new ArrayList<EditText>(0);
+	//Array of human values
+	private ArrayList<CheckBox> m_human = new ArrayList<CheckBox>(0);
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -17,14 +31,28 @@ public class NewGameActivity extends Activity
 	public void onFileSelected()
 	{
 		//TODO: Get filename
-		//TODO: Read file and find max players
+		//TODO: Read file and find numPlayers
 		
-		LinearLayout m_playerOptionsLayout = (LinearLayout) findViewById(R.id.playeroptionslayout);		
+		LinearLayout playerOptionsLayout = (LinearLayout) findViewById(R.id.playeroptionslayout);		
 		
 		//Loop through players and create a new view for each one
-		for(int i = 0; i < 5/*numPlayers*/; i++)
+		for(int i = 0; i < m_numPlayers; i++)
 		{
-			getLayoutInflater().inflate(R.layout.playeroption, m_playerOptionsLayout);
+			//Inflate the XML for the playeroption LinearLayout
+			getLayoutInflater().inflate(R.layout.playeroption, playerOptionsLayout);
+			//Add the corresponding views to the array for easy access
+			m_names.add((EditText) ((LinearLayout) playerOptionsLayout.getChildAt(i)).getChildAt(1));
+			m_human.add((CheckBox) ((LinearLayout) playerOptionsLayout.getChildAt(i)).getChildAt(2));
+		}
+	}
+	
+	public void createGame(View v)
+	{
+		Game createdGame;
+		ArrayList<Player> players = new ArrayList<Player>(0);
+		for(int i = 0; i < m_numPlayers; i++)
+		{
+			players.add(new Player(m_names.get(i).getText().toString(), m_human.get(i).isChecked()));
 		}
 	}
 }
